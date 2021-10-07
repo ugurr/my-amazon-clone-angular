0f91f3504b66d649c32394290b9a927ed5550f96
reyhan ugur <ugur_ryhn@hotmail.com>
2021-10-07 03:21:14 +0300

    My first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './PAGES/home/home.component';
+import { FooterComponent } from './COMPONENTS/footer/footer.component';
+import { CheckoutComponent } from './PAGES/checkout/checkout.component';
+import { LoginComponent } from './PAGES/login/login.component';
+import { HeaderComponent } from './COMPONENTS/header/header.component';
+import { BannerComponent } from './COMPONENTS/banner/banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+     HomeComponent,
+    FooterComponent,
+    CheckoutComponent,
+    LoginComponent,
+    HeaderComponent,
+    BannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
